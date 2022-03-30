a957ec9fd1f0bb5c2be2721d5479f8f91d0a5502
Mack Rusing <mackrusing@gmail.com>
2022-03-30 11:51:40 -0700

    Update separation

---
+++ helpers/string-conversions.js
@@ -0,0 +1,8 @@
+module.exports.measure = (measureStr) => {
+  const meaArr = meaStr.split(' ');
+  return {
+    ammount: +meaArr[0],
+    unit: meaArr[1],
+    formula: meaArr[2],
+  };
+};
--- main.js
+++ main.js
@@ -4,7 +4,19 @@ const input = require('helpers/user-input.js');
 // command line arguments
 const arguments = process.argv.slice(2);
 
-// handle no command line arguments
-if (arguments.length === 0) {
-  console.log('no ');
+// handle help command
+if (arguments[0] === 'help') {
+  console.log('avalible commands: help, solve');
+  process.exit(0);
 }
+
+// handle solve command
+if (arguments[0] === 'solve') {
+  process.exit(0);
+}
+
+// handle invalid command call
+console.log(
+  'no/invalid command provided. use help command to see list of operations'
+);
+process.exit(1);
