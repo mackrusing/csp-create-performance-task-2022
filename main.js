13bb61cd567ebf79cc161ef8ac4a06dc807ade11
Mack Rusing <mackrusing@gmail.com>
2022-03-30 10:35:17 -0700

    Update main

---
+++ main.js
@@ -0,0 +1,10 @@
+// helpers
+const input = require('helpers/user-input.js');
+
+// command line arguments
+const arguments = process.argv.slice(2);
+
+// handle no command line arguments
+if (arguments.length === 0) {
+  console.log('no ');
+}
