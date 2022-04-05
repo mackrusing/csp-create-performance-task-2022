36bdf4cc50c032d0bbb0c9b2218d9c8d3e43336a
Mack Rusing <mackrusing@gmail.com>
2022-04-04 17:14:40 -0700

    Add helper function

--- helpers/calculate.js
+++ helpers/calculate.js
@@ -1,19 +1,25 @@
 module.exports.molesToGrams = () => {};
 
+module.exports.gramsToMoles = () => {};
+
 module.exports.simpleStoich = (equation, input) => {
   const endTable = {};
 
   // add attributes to table
   for (const side of Object.keys(equation)) {
     for (const obj of equation[side]) {
-      endTable[obj.formula] = { b: 0, c: 0, a: 0 };
+      endTable[obj.formula] = { moles: 0, grams: 0 };
     }
+  }
+
+  // convert moles to grams
+  if (input.unit === 'grams') {
   }
 
   // add input
   endTable[input.formula].b = input.ammount;
 
-  // modify b column
+  // calculate moles column
   for (const equation of Object.keys(endTable)) {
   }
 
