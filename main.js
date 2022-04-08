43ea5f6757bbffbaaca3fd7f1e2cdc375364b3b9
Mack Rusing <mackrusing@gmail.com>
2022-04-07 19:18:57 -0700

    Add error handlers

--- main.js
+++ main.js
@@ -4,35 +4,21 @@
 const chalk = require('chalk');
 
 // handlers
-const solve = require('./handlers/solve');
+const { solve } = require('./handlers/solve');
 const { convert } = require('./handlers/convert');
-const help = require('./handlers/help');
+const { help } = require('./handlers/help');
 
 // command line arguments
 const args = process.argv.slice(2);
 
 // handle help command
 if (args[0] === 'help' || args.length === 0) {
-  help.usage();
+  help();
 }
 
 // handle solve command
 else if (args[0] === 'solve') {
-  if (args[1] === 'simple') {
-    solve.simple();
-  } else if (args[1] === 'lr') {
-    console.log('Solving limiting reactant problems is not supported yet.');
-  } else if (args[1] === 'help' || !args[1]) {
-    help.solve();
-  } else {
-    console.log(
-      `unknown command "${args[1]}" for "solve"`,
-      '\n',
-      '\nUse',
-      chalk.italic('chemhelp solve help'),
-      'to see a list of subcommands.'
-    );
-  }
+  solve();
 }
 
 // handle calculate command
