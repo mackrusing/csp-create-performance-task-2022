b589224e6381344b7a28cbe011ae8fab6b9a089a
Mack Rusing <mackrusing@gmail.com>
2022-04-08 21:35:48 -0700

    Update help command

--- handlers/help.js
+++ handlers/help.js
@@ -3,16 +3,21 @@ const chalk = require('chalk');
 
 module.exports.help = () => {
   console.log(
-    'Perform basic chemistry calculations.',
+    'A command-line tool that helps with basic chemistry calculations.',
     '\n',
     chalk.bold('\nUSAGE'),
-    '\n  chemhelp <command> <subcommand>',
+    '\n  chemhelp <command>',
     '\n',
     chalk.bold('\nCORE COMMANDS'),
-    '\n  solve:        Perform a stoichiometry calculation',
     '\n  convert:      Convert from grams to moles (and vice versa)',
+    '\n  solve:        Solve a simple stoich equation',
+    '\n  help:         Display this help menu',
     '\n',
-    chalk.bold('\nADDITIONAL COMMANDS'),
-    '\n  help:         Display help for a command'
+    chalk.bold('\nINPUT FORMATS'),
+    '\n  equations:    CH4 + 2O2 -> CO2 + 2H2O',
+    '\n                CH4 + 2O2 = CO2 + 2H2O',
+    '\n',
+    '\n  measures:     2.5 moles CO2',
+    '\n                235 grams CO2'
   );
 };
